9bcf7870ea598f291c94f3a67fc2e6808d9aa777
Gabriella Elizondo <gabriellaelizondo@gmail.com>
2022-12-11 21:28:03 -0500

    commit tree 1dd7d99cc30d29992a6296c4900c1661a463b064

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const startingBlock = 42;
+const someValue = 50;
+function distanceFromHqInBlocks(someValue) {
+  var dist = someValue - startingBlock;
+  return Math.abs(dist);
+}
+
+function distanceFromHqInFeet(someValue) {
+  return distanceFromHqInBlocks(someValue) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  var dist = (destination - start) * 264;
+  return dist < 0 ? -dist : dist;
+}
+
+function calculatesFarePrice(start, destination) {
+  var distanceFeet = distanceTravelledInFeet(start, destination);
+  if (distanceFeet <= 400) {
+    return 0;
+  } else if (distanceFeet >= 400 && distanceFeet <= 2000) {
+    return (distanceFeet - 400) * 0.02;
+  } else if (distanceFeet > 2000 && distanceFeet < 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
